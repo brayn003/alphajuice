7456d2b9e912fa826bebf40551f4bdb9a9777573
brayn003 <brayn003@gmail.com>
2016-07-01 11:54:58 +0530

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat'),
+	browserSync = require('browser-sync').create();
+
+var lib = {
+		'vendor': {
+			'js' : [
+				'./bower_components/angular/angular.js',
+				'./bower_components/phaser/phaser.js'
+			]
+		}
+	};
+		
+gulp.task('vendor',function(){
+	gulp.src(lib.vendor.js)
+	.pipe(concat('vendor.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('./app/js/'));
+});
+
+gulp.task('html',function(){
+	gulp.src('./templates/**/*.html')
+	.pipe(gulp.dest('./app/templates/'));
+	// .pipe(browserSync.reload());
+	browserSync.reload();
+	// .pipe(browserSync.reload());
+});
+
+gulp.task('sass',function(){
+	gulp.src('./sass/*.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(gulp.dest('./app/css/'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('js',function(){
+	gulp.src('./templates/**/*.js')
+	.pipe(uglify())
+	.pipe(gulp.dest('./app/templates/'));
+	browserSync.reload();
+	// .pipe(browserSync.stream());
+});
+
+gulp.task('ijs',function(){
+	gulp.src('./js/**/*.js')
+	// .pipe(uglify())
+	.pipe(gulp.dest('./app/js/'));
+	browserSync.reload();
+	// .pipe(browserSync.stream());
+});
+
+gulp.task('serve',function(){
+	browserSync.init({
+		'server': {
+			'baseDir': './app/'
+		}
+	});
+	gulp.watch('./templates/**/*.html', ['html']);
+	gulp.watch('./sass/*.scss', ['sass']);
+	gulp.watch('./templates/*.js', ['js']);
+	gulp.watch('./js/**/*.js', ['ijs']);
+});
+
+
+gulp.task('default',['vendor']);
---
+++ js/app.js
@@ -0,0 +1,81 @@
+var app = angular.module('alphaJuice', []);
+
+app.controller('appCtrl',['$scope','$interval','$log','$http',function($scope,$interval,$log,$http){
+	$scope.audience = {
+		'request' : []
+	};
+	
+	$scope.blender = {
+		'word' : ''
+	};
+
+	$scope.dictionary = {
+
+	};
+
+	$scope.audience.startRequest = startRequest;
+	$scope.blender.indexof = indexof;
+
+	$http.get('wordlist.json').success(function(data) {
+	    $scope.dictionary = data;
+	    alert($scope.dictionary.indexOf('hi'));
+	});
+	var requestInterval;
+	var generating = 0;
+
+	// generic function
+	function generateAlphabet(){
+		return String.fromCharCode(97 + Math.floor(Math.random() * 26));
+	};
+
+	function generateNumber(){
+		return 1 + Math.floor(Math.random() * 36);
+	};
+
+	
+	// non generic funtions
+	function startRequest(){
+		if(generating == 1){
+			return;
+		}
+		generating = 1;
+		requestInterval = $interval(generateRequestArray, 300);
+		generating = 0;
+							// .then(function(){
+							// 	requestInterval = undefined;
+							// });
+	};
+
+	function generateRequestArray(){
+		var key = generateNumber();
+		var value = generateAlphabet();
+		var limit = 10;
+		$log.log(key+" : "+value);
+		// $log.log($scope.audience.request.filter(function(x){ return true }).length);
+
+		if($scope.audience.request.filter(function(x){ return true }).length > limit-1) {
+			$log.log('Stopping');
+			$interval.cancel(requestInterval);
+		}else{
+			if($scope.audience.request[key] === null || $scope.audience.request[key] === undefined){
+				$scope.audience.request[key]= value;
+			}
+		}
+
+		
+		// else {
+		// 	// generateRequestArray();
+		// }
+		// alert(JSON.stringify(req)); 
+	};
+
+	function indexof(){
+		for (var i = 0; i < $scope.blender.word.length; i++) {
+			delete $scope.audience.request[$scope.audience.request.indexOf($scope.blender.word[i])];
+		}
+		startRequest();
+	};
+
+	startRequest();
+
+}])
